b34b08016129b317f7b098c9623f5f2ccecb1175
Franco Morales Castro <franco.morales@outlook.com>
2022-05-23 18:00:49 -0400

    feat: initial project

---
+++ src/main/java/com/proyectospormfinal/proyectospormfinal/ProyectospormfinalApplication.java
@@ -0,0 +1,13 @@
+package com.proyectospormfinal.proyectospormfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectospormfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectospormfinalApplication.class, args);
+	}
+
+}
